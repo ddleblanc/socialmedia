eff8ccf2b15020e2e88f091b0a5280bbf5e7ee88
unknown <lxblvnc@gmail.com>
2019-04-23 09:49:49 +0200

    bug fixes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport");
 const db = require("./db/index");
-const compression = require("compression");
 
 process.setMaxListeners(0);
 

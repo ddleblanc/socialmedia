c5c3c436b67805312d7f42318ba01a3228ac9ca2
ddleblanc <lxblvnc@gmail.com>
2019-08-21 23:39:12 +0200

    added intercom button

--- angular-src/src/app/components/user-detail/user-detail.component.ts
+++ angular-src/src/app/components/user-detail/user-detail.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
-import { User } from 'src/app/models/user.model';
-import { trigger, transition, style, animate } from '@angular/animations';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AuthService } from "src/app/services/auth.service";
+import { User } from "src/app/models/user.model";
+import { trigger, transition, style, animate } from "@angular/animations";
 import { Location } from "@angular/common";
-import { FollowService } from 'src/app/services/follow.service';
-import { environment } from '../../../environments/environment';
+import { FollowService } from "src/app/services/follow.service";
+import { environment } from "../../../environments/environment";
 
 @Component({
-  selector: 'app-user-detail',
-  templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.scss'],
+  selector: "app-user-detail",
+  templateUrl: "./user-detail.component.html",
+  styleUrls: ["./user-detail.component.scss"],
   animations: [
     trigger("enterAnimation", [
       transition(":enter", [
         style({ opacity: 0 }),
         animate("500ms ease-in-out", style({ opacity: 1 }))
+      ])
+    ]),
+    trigger("btnEnterAnimation", [
+      transition(":enter", [
+        style({ transform: "scale(0)" }),
+        animate("220ms 1500ms ease-in-out", style({ transform: "scale(1)" }))
       ])
     ]),
     trigger("profilePicAnimation", [
@@ -64,11 +70,18 @@ export class UserDetailComponent implements OnInit {
   public user: User;
   private accountUser: User;
   currentPostNumber = -1;
+  IsIntercomOpen = false;
 
-  constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute, private _location: Location, private followService: FollowService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private _location: Location,
+    private followService: FollowService
+  ) {}
 
   ngOnInit() {
-    this.getUsers()
+    this.getUsers();
   }
 
   onBackClicked() {
@@ -78,49 +91,64 @@ export class UserDetailComponent implements OnInit {
   // Scroll behavior
   scrollToNextPost() {
     if (this.currentPostNumber < this.user.posts.length - 1) {
-      this.currentPostNumber++
-      let el = document.getElementById(`post_${this.currentPostNumber}`)
-      el.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+      this.currentPostNumber++;
+      let el = document.getElementById(`post_${this.currentPostNumber}`);
+      el.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest"
+      });
     } else {
-      console.log("Bottom reached")
+      console.log("Bottom reached");
     }
   }
 
   scrollToPreviousPost() {
     if (this.currentPostNumber != -1) {
-      console.log(this.currentPostNumber)
+      console.log(this.currentPostNumber);
       if (this.currentPostNumber == 0) {
-        this.currentPostNumber--
-        let top = document.getElementById(`user-detail-container`)
-        top.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
-        console.log("Top reached")
+        this.currentPostNumber--;
+        let top = document.getElementById(`user-detail-container`);
+        top.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+          inline: "nearest"
+        });
+        console.log("Top reached");
       } else {
-        this.currentPostNumber--
-        let el = document.getElementById(`post_${this.currentPostNumber}`)
-        el.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        this.currentPostNumber--;
+        let el = document.getElementById(`post_${this.currentPostNumber}`);
+        el.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+          inline: "nearest"
+        });
       }
     }
   }
 
   // Get user accounts
   getUsers() {
-    this.route.params.subscribe(params => { this.username = params.username })
+    this.route.params.subscribe(params => {
+      this.username = params.username;
+    });
     this.authService.getUserByUsername(this.username).subscribe(data => {
       if (data.success) {
         this.user = data.user;
         this.avatar = `${environment.pathToPhotos}${data.user.avatar}`;
         // console.log(data)
         if (data.user.posts.length > 0) {
-          this.wallpaper = `${environment.pathToPhotos}${data.user.posts[0].photo}`;
+          this.wallpaper = `${environment.pathToPhotos}${
+            data.user.posts[0].photo
+          }`;
         } else {
           this.wallpaper = `${environment.pathToPhotos}${data.user.avatar}`;
         }
-        this.getAccountUser()
+        this.getAccountUser();
         console.log(`user: ${this.user.email}`);
       } else {
-        this.router.navigate(['/'])
+        this.router.navigate(["/"]);
       }
-
     });
   }
   getAccountUser() {
@@ -130,12 +158,11 @@ export class UserDetailComponent implements OnInit {
       this.accountUser = data.user;
       if (this.accountUser.following.includes(this.user._id)) {
         this.following = true;
-        console.log("following")
+        console.log("following");
       } else {
         this.following = false;
-        console.log("not following")
+        console.log("not following");
       }
-
     });
   }
 
@@ -143,34 +170,41 @@ export class UserDetailComponent implements OnInit {
   onFollow() {
     let theirId = this.user._id;
     let userId = { userId: this.accountUser._id };
-    console.log(this.user)
+    console.log(this.user);
     if (this.user._id != this.accountUser._id) {
       if (!this.accountUser.following.includes(theirId)) {
-        console.log("t: " + theirId)
-        console.log("i: " + userId)
-        this.followService.addUserToFollowing(theirId, userId).subscribe(data => {
-          this.data = data;
-          if (this.data.success) {
-            this.ngOnInit();
-          } else {
-            // this.router.navigate(['/'])
-            console.log("failed");
-          }
-        });
+        console.log("t: " + theirId);
+        console.log("i: " + userId);
+        this.followService
+          .addUserToFollowing(theirId, userId)
+          .subscribe(data => {
+            this.data = data;
+            if (this.data.success) {
+              this.ngOnInit();
+            } else {
+              // this.router.navigate(['/'])
+              console.log("failed");
+            }
+          });
       } else {
-        console.log("t: " + theirId)
-        console.log("i: " + userId)
-        this.followService.removeUserFromFollowing(theirId, userId).subscribe(data => {
-          this.data = data;
-          if (this.data.success) {
-            this.ngOnInit()
-          } else {
-            // this.router.navigate(['/'])
-            console.log("failed");
-          }
-        });
+        console.log("t: " + theirId);
+        console.log("i: " + userId);
+        this.followService
+          .removeUserFromFollowing(theirId, userId)
+          .subscribe(data => {
+            this.data = data;
+            if (this.data.success) {
+              this.ngOnInit();
+            } else {
+              // this.router.navigate(['/'])
+              console.log("failed");
+            }
+          });
       }
-
     }
+  }
+  onIntercomClicked() {
+    console.log("clicked");
+    this.IsIntercomOpen = !this.IsIntercomOpen;
   }
 }
